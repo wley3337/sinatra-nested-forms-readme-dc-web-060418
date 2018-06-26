9d79047325396ecc7ea64ac0da0eab3a1b5d6ccc
wley3337 <wley3337@gmail.com>
2018-06-26 11:38:12 -0400

    notes for furthe understanding

--- app.rb
+++ app.rb
@@ -2,4 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+  get "/" do
+    erb :new
+  end
+
+  post "/student" do
+    @student = Student.new(params[:student]) #<--This takes all of the paramaters within student key value and passes them in to create a new student
+      params[:student][:courses].each do |details| #<--this iterates over all of the hashes in the array of courses from the form and creates a new course object with those details 
+        Course.new(details)
+      end
+
+      @courses = Course.all
+      erb :student
+  end
+
 end
